84083d96eecc0d4c9d2849b46560150d1826fa74
vera_chernevich <9111402516v@gmail.com>
2025-01-21 17:37:31 +0300

    feat: Add mobile menu with dropdowns, improve responsiveness and create docs folder

--- src/js/index.js
+++ src/js/index.js
@@ -5,17 +5,37 @@ import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
-	slidesPerView: 3,
-	spaceBetween: 24,
+	slidesPerView: 1,
+	spaceBetween: 5,
 	loop: true,
-  
+
+	breakpoints: {
+		450: {
+			slidesPerView: 2,
+			spaceBetween: 16,
+		},
+		780: {
+			slidesPerView: 3,
+			spaceBetween: 24,
+		}
+	},
+
 	// If we need pagination
 	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
+		el: '.swiper-pagination',
+		clickable: true,
 	},
-  });
+});
+
+//галлерея follow
+
+
 // Мобильная навигация
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+// Аккордеон для мобильной навигации
+import accordion from './modules/accordion.js';
+accordion();
+
 
